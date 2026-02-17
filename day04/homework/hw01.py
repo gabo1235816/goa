b17bfca4afadb7e220196b5767abc8e257e47072
gabo1235816 <oqropiridzegio61@gmail.com>
2026-02-17 20:40:08 +0400

    1

---
+++ day04/homework/hw01.py
@@ -0,0 +1,22 @@
+#2)შექმენი 5 ცვლადი name , surname  , address , hobby , fav_movie ამ ცვლადებში მომხმარებელს შემოატანინე შესაბამისი ინფორმაცია input()-ის მეშვეობით და შემდეგ დაბეჭდე ტერმინალში სრული და გამართული წინადადება ამ ცვლადების გამოყენე
+
+print()
+
+
+user_name = input("Enter your user here!")
+
+print(user_name)
+
+sur_name = input("Oqropiridze")
+
+print("sur_name" +user_name)
+
+adress = input("Gori")
+
+print(adress)
+
+hobby = input("Soccer")
+
+print(hobby)
+
+fav_movie = input("Stranger things")
