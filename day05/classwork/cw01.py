9761a3945460dce4726a8010dd41d12d65501b04
gabo1235816 <oqropiridzegio61@gmail.com>
2026-02-24 20:14:16 +0400

    Create cw01.py

---
+++ day05/classwork/cw01.py
@@ -0,0 +1,8 @@
+#2) მომხმარებელს მოთხოვეთ თავისი დაბადების წელი და გაიგეთ რამდენი წლისაა იგი
+
+
+age = input('enter your age here!: ')
+print(age)
+
+birthday = input('enter your birthday here!: ')
+print(birthday)
